a0a469d84c579559adff19b216a2f9f4198f4013
Daniel Pessoto <d.pessoto@hotmail.com>
2021-09-16 14:08:51 -0300

    initial commit

---
+++ app/src/main/java/pessoto/android/mobile/challenge/btg/MainActivity.kt
@@ -0,0 +1,11 @@
+package pessoto.android.mobile.challenge.btg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
